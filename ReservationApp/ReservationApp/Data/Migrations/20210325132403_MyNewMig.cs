182501b9275fa7ac9c2918a0c1bf72551210cfc9
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2021-03-25 14:32:04 +0100

    Models && database contection

---
+++ ReservationApp/ReservationApp/Data/Migrations/20210325132403_MyNewMig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReservationApp.Data.Migrations
+{
+    public partial class MyNewMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ ReservationApp/ReservationApp/Models/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationApp.Models
+{
+    public class Reservation
+    {
+
+        public string id { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+        public string Cause { get; set; }
+        public Student Student { get; set; }
+        public TypeReservation Reserve { get; set; }
+    }
+}
